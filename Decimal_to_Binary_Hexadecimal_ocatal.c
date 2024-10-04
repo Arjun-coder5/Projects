c10e045cb9f52f866230c0c69f6e20e0f763e837
Arjun-coder5 <arjunsorout526@gmail.com>
2024-10-04 14:28:09 +0530

    Add files via upload

---
+++ Decimal_to_Binary_Hexadecimal_ocatal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int decimalNumber;
+
+    // Input: decimal number from the user
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimalNumber);
+
+    // For binary representation
+    printf("Binary: ");
+    for (int i = 31; i >= 0; i--) {
+        int binaryBit = (decimalNumber >> i) & 1; 
+        printf("%d", binaryBit);
+    }
+    printf("\n");
+
+    // For hexadecimal representation
+    printf("Hexadecimal: %X\n", decimalNumber);
+
+    // For octal representation
+    printf("Octal: %o\n", decimalNumber);
+
+    return 0;
+}
